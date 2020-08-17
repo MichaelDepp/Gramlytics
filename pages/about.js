b211ec2d13d3a390fcf2822466d87fd9f03ff516
Michael Depp <michaeldepp7@gmail.com>
2020-08-17 18:38:35 +0800

    second update

--- pages/about.js
+++ pages/about.js
@@ -7,11 +7,13 @@ const About = () => (
             <div className="write-content">
                 <h1>About</h1>
                 <div className="paragraph">
-                    <p>Gramlytics is a Instagram Analytics Web Application. This application search any username profile that exists in Instagram
-                    and scrappes the information of that profile and give you a detailed analytics about the profile. Gramlytics can't fetch private profile's
-                    information. The application will give you informations like total posts, followers, following, total likes, total comments,
-                    average likes, average comments, most liked posts (9 posts), most commented posts (9 posts), and also a bar graph illustrating
-                the daily analytics which can indicate the profile's high traffic days and low traffic days.</p>
+                    <p>Gramlytics is an Instagram Analytics Web Application. This application search any 
+                    username profile that exists in Instagram and scrapes the information of that profile 
+                    and give you a detailed analytics about the profile. Gramlytics can't fetch private profile's
+                    information. The application will give you information like total posts, followers, following,
+                    total likes, total comments, average likes, average comments, most liked posts (9 posts), most 
+                    commented posts (9 posts), and also a bar graph illustrating the daily analytics which can show
+                    the profile's high traffic days and low traffic days.</p>
                 </div>
             </div>
         </Content>
--- pages/github.js
+++ pages/github.js
@@ -9,8 +9,11 @@ const Github = () => (
                 <div className="paragraph">
                     <p>Gramlytics is a Instagram Analytics Web Application. This application is fully build by Michael.
                     This application's frontend was build in Next.js (React Library's Framework). The backend of this application uses
-                    Express.js which is a flexible and robust Node.js's web application Framwork. The full source code for this application
-                is avaible on Github. You can fork the repository and freely experiment or contribute for this simple project.</p>
+                    Express.js which is a flexible and robust Node.js's web application Framework. The full source code for this application
+                    is avaible on Github. You can fork the repository and freely experiment or contribute for this simple project.</p>
+                </div>
+                <div className="icon">
+                    <a href="https://github.com/MichaelDepp/Gramlytics" target="_blank"><img src="./assets/github.png"/></a>
                 </div>
             </div>
         </Content>
